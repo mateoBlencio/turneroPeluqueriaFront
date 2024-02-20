5342acc22fc24dcbd57d53c9f0f4aed1b5bf13ca
Mateo <90317@sistemas.frc.utn.edu.ar>
2024-02-20 15:28:12 -0300

    intento de login

--- App.js
+++ App.js
@@ -1,63 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, ScrollView } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  ScrollView,
+  TextInput,
+  Button,
+  TextBase,
+} from "react-native";
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
 
+  /*
   useEffect(() => {
-    fetch('http://192.168.1.105:8080/roles')
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("http://192.168.1.105:8080/roles")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
+  */
 
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.text}>Número: {item.numero}</Text>
-      <Text style={styles.text}>Nombre: {item.nombre}</Text>
-    </View>
-  );
+  const handleLogin = (email, pass) => {
+    alert(`Iniciando sesión con email: ${email} y contraseña: ${pass}`);
+  };
 
   return (
-    <SafeAreaView  style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>Usuarios</Text>
+        <Text style={styles.headerText}>Bienvenido!</Text>
       </View>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.numero.toString()}
-      />
-    </SafeAreaView >
+
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Mail" value={mail} onChangeText={(text) => setMail(text)}/>
+        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} value={password} onChangeText={(text) => setPassword(text)}/>
+        <Text style={styles.textButtoms}>No tiene cuenta? <Text style={{textDecorationLine: 'underline',color: 'blue'}}>
+          Registrarse</Text></Text>
+        <Button title="Iniciar Sesión" onPress={() => handleLogin(mail, password)} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: 'gray',
+    backgroundColor: "#fff",
   },
   header: {
-    backgroundColor: '#87CEFA',
-    padding: 10,
-    alignItems: 'center',
+    backgroundColor: "#87CEFA",
+    paddingTop: 40,
+    paddingBottom: 15,
+    alignItems: "center",
   },
   headerText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#000',
+    fontWeight: "bold",
   },
-  item: {
-    backgroundColor: '#87CEFA',
+  inputContainer: {
     padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10
   },
-  title: {
-    fontSize: 25,
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
   },
-
+  textButtoms: {
+    padding: 10,
+    textAlign: "center"
+  }
 });
