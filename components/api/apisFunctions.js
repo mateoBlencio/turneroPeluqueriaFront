a4a21f626f1440a002af7967ce8cd6d1beade811
Mateo <90317@sistemas.frc.utn.edu.ar>
2024-03-08 01:11:18 -0300

    quedo sin funcionar para seleccionar la fecha, siempre selecciona la misma

--- components/api/apisFunctions.js
+++ components/api/apisFunctions.js
@@ -1,7 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import BASE_URL from "./urls";
 
-
 export const login = async (mailP, passwordP) => {
   const url = `${BASE_URL}/auth/login`;
 
@@ -17,6 +16,7 @@ export const login = async (mailP, passwordP) => {
     });
 
     if (!response.ok) {
+      alert("Algo salio mal");
       throw new Error("Network response was not ok");
     }
 
@@ -89,3 +89,55 @@ export const getTiposTurnos = async () => {
     throw new Error("Error fetching data: " + error.message);
   }
 };
+
+// public ResponseEntity<Object> getTurnosDisponiblesForPeluquero(@RequestParam Long dniPeluquero,
+// @RequestParam int day,
+// @RequestParam int month,
+// @RequestParam int year){
+//   try{
+//       int day = (int) requestBody.get("day");
+//       int month = (int) requestBody.get("month");
+//       int year = (int) requestBody.get("year");
+//       LocalDate fecha = LocalDate.of(year, month, day);
+
+//       Long dni = Long.valueOf((Integer) requestBody.get("dni"));
+
+//       val horas = turnoService.getHorariosLibresPorPeluquero(fecha, dni)
+//               .stream()
+//               .map(HorasResponse::from);
+//       return ResponseEntity.ok(horas);
+//   } catch (Exception e){
+//       return ResponseEntity.notFound().build();
+//   }
+// }
+
+export const getHorasLibre = async (dniPeluquero, nroTipoTurno, day, month, year) => {
+  const url = `${BASE_URL}/turnos/libres?dniPeluquero=${dniPeluquero}&day=${day}&month=${month}&year=${year}`;
+  
+  alert(`${day}/${month}/${year}`)
+
+  // Obtener el JWT guardado en AsyncStorage
+  const jwtToken = await AsyncStorage.getItem("jwt");
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        // Agregar el token JWT al encabezado de la solicitud
+        "Authorization": `Bearer ${jwtToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    // Devolver el listado de JSONs
+    return data;
+  } catch (error) {
+    throw new Error("Error fetching data: " + error.message);
+  }
+}
--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import RNPickerSelect from 'react-native-picker-select';
-import { getPeluqueros, getTiposTurnos } from "../api/apisFunctions";
-
+import RNPickerSelect from "react-native-picker-select";
+import {
+  getPeluqueros,
+  getTiposTurnos,
+  getHorasLibre,
+} from "../api/apisFunctions";
 
 function HomeScreen() {
-  const [date, setDate] = useState(new Date());
   const [showPicker, setShowPicker] = useState(false);
+  const [seleccionoFecha, setSeleccionoFecha] = useState(false);
+  const [date, setDate] = useState(new Date());
+
+  // Variable que luego se utilizan para enviarselas al back
+  const [dniPeluquero, setDniPeluquero] = useState();
+  const [nroTipoTurno, setNroTipoTurno] = useState();
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
-  const [dniPeluquero, setDniPeluquero] = useState();
-  const [seleccionoFecha, setSeleccionoFecha] = useState(false);
+  const [horaTurno, setHoraTurno] = useState();
+
+  // Variables que se utilizan en la obtencion de datos
   const [peluqueros, setPeluqueros] = useState([]);
   const [tiposTurnos, setTiposTurnos] = useState([]);
-
+  const [horasLibres, setHorasLibres] = useState([]);
 
   useEffect(async () => {
     const fetchData = async () => {
@@ -27,95 +36,108 @@ function HomeScreen() {
       }
 
       try {
-        const tiposTurnosData = await getTiposTurnos();
+        const tiposTurnosData = await getTiposTurnos(
+          dniPeluquero,
+          nroTipoTurno
+        );
         setTiposTurnos(tiposTurnosData);
       } catch (error) {
         console.error(error.message);
       }
-
-      try {
-        const tiposTurnosData = await getTiposTurnos();
-        setTiposTurnos(tiposTurnosData);
-      } catch (error) {
-        console.error(error.message);
-      }
-
     };
     fetchData();
   }, []);
 
-
-  const onChange = (event, selectedDate) => {
+  const onChange = async (event, selectedDate) => {
     const currentDate = selectedDate || date;
     setShowPicker(false);
     setDate(currentDate);
-    
-
+  
     // Extraer día, mes y año de la fecha seleccionada
     const selectedDay = currentDate.getDate();
-    const selectedMonth = currentDate.getMonth() + 1; // Los meses comienzan desde 0
+    const selectedMonth = currentDate.getMonth() + 1;
     const selectedYear = currentDate.getFullYear();
-
+  
     // Guardar los valores en el estado
-    setDay(selectedDay.toString());
-    setMonth(selectedMonth.toString());
-    setYear(selectedYear.toString());
+    setDay(selectedDay);
+    setMonth(selectedMonth);
+    setYear(selectedYear);
+  
+    try {
+      const horasLibresData = await getHorasLibre(
+        dniPeluquero,
+        nroTipoTurno,
+        selectedDay,
+        selectedMonth,
+        selectedYear
+      );
+      setHorasLibres(horasLibresData);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
+  
 
   const showDatepicker = () => {
     setShowPicker(true);
     setSeleccionoFecha(true);
   };
 
-  
-
   return (
     <View style={styles.container}>
       <View style={styles.misTurnosContainer}>
         <Text style={styles.title}>Mis turnos</Text>
-        <Text style={styles.normalText} >Aca va a haber una lista de los turnos</Text>
+        <Text style={styles.normalText}>
+          Aca va a haber una lista de los turnos
+        </Text>
       </View>
 
       <View style={styles.nuevoTurnoContainer}>
         <Text style={styles.title}>Nuevo turno </Text>
 
         <View style={styles.nuevoTurnoTematicaContainer}>
-          <Text style={styles.normalText} >Seleccionar peluquero</Text>
+          <Text style={styles.normalText}>Seleccionar peluquero</Text>
           <RNPickerSelect
             placeholder={{
-              label: 'Seleccione un peluquero',
-              value: null}}
+              label: "Seleccione un peluquero",
+              value: null,
+            }}
             onValueChange={(value) => setDniPeluquero(value)}
             items={peluqueros.map((peluquero) => ({
               label: peluquero.nombre,
-              value: peluquero.dni
+              value: peluquero.dni,
             }))}
           />
         </View>
 
         <View style={styles.nuevoTurnoTematicaContainer}>
-          <Text style={styles.normalText} >Seleccionar tipo de servicio:</Text>
+          <Text style={styles.normalText}>Seleccionar tipo de servicio:</Text>
           <RNPickerSelect
             placeholder={{
-              label: 'Seleccione un servicio',
-              value: null}}
-            onValueChange={(value) => setDniPeluquero(value)}
+              label: "Seleccione un servicio",
+              value: null,
+            }}
+            onValueChange={(value) => setNroTipoTurno(value)}
             items={tiposTurnos.map((tipoTurno) => ({
               label: tipoTurno.nombre,
-              value: tipoTurno.numero
+              value: tipoTurno.numero,
             }))}
           />
         </View>
 
         <View style={styles.nuevoTurnoTematicaContainer}>
-          <Text style={styles.normalText} >Fecha de turno:    
-        
-          {seleccionoFecha &&
-            <Text style={{fontSize:styles.normalText.fontSize, fontStyle: "italic"}} >
-              {day}/{month}/{year}
-            </Text>
-          }
-        
+          <Text style={styles.normalText}>
+            Fecha de turno:{" "}
+            {seleccionoFecha && (
+              <Text
+                style={{
+                  fontSize: styles.normalText.fontSize,
+                  fontStyle: "italic",
+                }}
+              >
+                {day}/{month}/{year}
+              </Text>
+            )}
           </Text>
           <Button onPress={showDatepicker} title={"Seleccionar fecha"} />
           {showPicker && (
@@ -123,34 +145,36 @@ function HomeScreen() {
               minimumDate={new Date()}
               value={date}
               mode="date"
-              onChange={onChange}
+              onChange={(v) => onChange(v)}
             />
           )}
-          
         </View>
 
         <View style={styles.nuevoTurnoTematicaContainer}>
-          <Text style={styles.normalText} >Horarios disponibles</Text>
+          <Text style={styles.normalText}>Horarios disponibles</Text>
           <RNPickerSelect
             placeholder={{
-              label: 'Seleccione un horario',
-              value: null}}
+              label: "Seleccione un horario",
+              value: null,
+            }}
             onValueChange={(value) => setHoraTurno(value)}
-            items={horarios.map((horario) => ({
+            items={horasLibres.map((horario) => ({
               label: horario.horaData,
-              value: horario.hora
+              value: horario.hora,
             }))}
           />
         </View>
-            
+
         <View>
-          <Button title="Pedir turno" color={"green"}/>
+          <Button title="Pedir turno" color={"green"} />
         </View>
       </View>
 
       <View style={styles.turnosAnterioresContainer}>
         <Text style={styles.title}>Turnos recientes</Text>
-        <Text style={styles.normalText}>Aca va a haber una lista de los ultimos 3 turnos</Text>
+        <Text style={styles.normalText}>
+          Aca va a haber una lista de los ultimos 3 turnos
+        </Text>
       </View>
     </View>
   );
@@ -160,15 +184,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f0f0f0",
-    marginHorizontal: 10
+    marginHorizontal: 10,
   },
   title: {
     fontSize: 30,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   normalText: {
     fontSize: 15,
-    
   },
   misTurnosContainer: {
     marginVertical: 15,
@@ -176,7 +199,7 @@ const styles = StyleSheet.create({
   nuevoTurnoContainer: {
     marginVertical: 20,
   },
-  nuevoTurnoTematicaContainer:{
+  nuevoTurnoTematicaContainer: {
     marginVertical: 5,
   },
   turnosAnterioresContainer: {
